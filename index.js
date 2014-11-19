4db06cfeee06f658b619c0ab8ffd9a2cefa7ffa6
Tim Suchanek <tim.suchanek@gmail.com>
2014-11-19 14:37:15 +0100

    Add index.js

---
+++ index.js
@@ -0,0 +1,62 @@
+var support = require('./utils').support,
+    dispatchClick = require("./utils").dispatchClick;
+
+if (typeof document.addEventListener !== 'function') {
+    throw new Error('document.addEventListener doesnt exist');
+}
+
+module.exports = function(ele) {
+    ele.addEventListener('DOMContentLoaded', function() {
+        if(!support) {
+            throw new Error('We\'re not in the browser doesnt exist');
+        }
+
+        var cover = document.createElement('div'),
+            body = document.body,
+            coverStyle = cover.style,
+            scrollStarted = false,
+            timer,
+            clicked = false,
+            pos = { x: 0, y: 0 };
+
+        coverStyle.cssText = [
+            '-webkit-transform: translate3d(0,0,0);',
+            'transform: translate3d(0,0,0);',
+            'position: fixed;',
+            'top: 0;',
+            'right: 0;',
+            'left: 0;',
+            'bottom: 0;',
+            'opacity: 0;',
+            'z-index: 9;',
+            'pointer-events: none'
+        ].join('');
+        body.appendChild(cover);
+
+        window.addEventListener('scroll', function scroll() {
+            if(!scrollStarted) {
+                coverStyle.pointerEvents = 'auto';
+                scrollStarted = true;
+            }
+            clearTimeout(timer);
+
+            timer = setTimeout(function(){
+                coverStyle.pointerEvents = 'none';
+                scrollStarted = false;
+                if(clicked) {
+                    dispatchClick(pos);
+                    clicked = false;
+                }
+            }, 500);
+        }, false);
+
+        // capture all clicks and store x, y coords for later
+        document.addEventListener('click', function clickCatcher(event) {
+            if(event.target === cover && !event.synthetic) {
+                pos.x = event.clientX;
+                pos.y = event.clientY;
+                clicked = true;
+            }
+        }, false);
+    }, false);
+}
